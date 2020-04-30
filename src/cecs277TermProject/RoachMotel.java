91e89aa62ff9379fde24dd04a0a78ceac2dce826
Tyler V <49462625+tylerdvanl@users.noreply.github.com>
2020-04-30 01:32:16 -0700

    created another file
    It's late and this project is gonna take some time to understand

---
+++ src/cecs277TermProject/RoachMotel.java
@@ -0,0 +1,13 @@
+package cecs277TermProject;
+
+import java.util.ArrayList;
+
+
+//I will work on this later, it's late
+public class RoachMotel
+{
+	private int capacity;
+	private boolean vacancy;
+	private ArrayList<Integer> availableRooms;
+	private String name;
+}
