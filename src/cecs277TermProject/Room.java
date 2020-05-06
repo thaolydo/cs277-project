54d020c67e6f70666c5ac3b087628fd41b914ad9
Gustavo Pech <31997991+GEP-X@users.noreply.github.com>
2020-05-06 03:37:18 -0700

    Updated Room.java
    I changed it from abstract since I think there should be multiple instances of each room.
    I also made a dummy cost() class until we get the actual numbers.

--- src/cecs277TermProject/Room.java
+++ src/cecs277TermProject/Room.java
@@ -1,20 +1,24 @@
 package cecs277TermProject;
 
-public abstract class Room {
+public class Room {
     protected int roomNumber;
     protected RoomType type;
-    protected double baseRate;
 
     public Room(int roomNumber, RoomType type) {
         this.roomNumber = roomNumber;
         this.type = type;
     }
 
-
-    public abstract double getCost();
-
     @Override
     public String toString() {
         return String.format("%d", roomNumber);
     }
+
+	public  double getCost() {
+		return 0;
+	}
+    
+
+    
+
 }
