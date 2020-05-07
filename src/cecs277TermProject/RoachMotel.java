d49d916acf08e1e720fcafa74112a51d11df06b1
Ly Do <lydo1797@gmail.com>
2020-05-06 17:46:13 -0700

    Ly: Implement reading logs in main(). Move System.out.println out of RoachMotel to main().

--- src/cecs277TermProject/RoachMotel.java
+++ src/cecs277TermProject/RoachMotel.java
@@ -19,7 +19,7 @@ public class RoachMotel {
 	private String name;
 	private int capacity; // This is a dummy variable because it's not used anywhere
 	private Logger logger;
-	
+
 
 	// TODO: [Gust] Make this constructor private and use it in Singleton pattern
 	public static RoachMotel getRoachMotel() throws FileNotFoundException {
@@ -57,8 +57,6 @@ public class RoachMotel {
 			logger.log(String.format("Colony %1$s failed to check in, no available rooms.", roachColony));
 			return result + "No available room.";
 		}
-		// TODO: [Ly] Implement checkIn validation
-		System.out.printf("Checking in: %s\n", roachColony);
 		Room room = availableRooms.poll();
 		occupiedRooms.put(roachColony, room);
 		logger.log(String.format("Successfully Checked In: Colony %1$s checking into %2$s", roachColony, occupiedRooms.get(roachColony)));
@@ -68,13 +66,12 @@ public class RoachMotel {
 	}
 
 	public void checkOut(RoachColony roachColony, PaymentStrategy paymentMethod, int numDays) {
-		logger.log(String.format("Successfully Checked Out: Colony %1$s checking out of %2$s cost: %3$s using %4$s", 
+		logger.log(String.format("Successfully Checked Out: Colony %1$s checking out of %2$s cost: %3$s using %4$s",
 					roachColony, occupiedRooms.get(roachColony), (occupiedRooms.get(roachColony).getCost() * numDays), paymentMethod));
-		System.out.printf("Checking out: %s\n", roachColony);
-		this.pay(paymentMethod, roachColony, numDays);	
+		this.pay(paymentMethod, roachColony, numDays);
 		availableRooms.add(occupiedRooms.remove(roachColony));
 	}
-	
+
 	/**
 	 * Will allow a colony to pay for the room upon checkout.  A payment method (RoachPal or MasterRoach) will be passed in through the main method.
 	 * @param paymentMethod  A payment method (RoachPal or MasterRoach) passed in through the main method.
